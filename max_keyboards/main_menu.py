95e35c6b08aa27294713897b54f3222040f80884
NNFall <100708167+NNFall@users.noreply.github.com>
2026-05-29 00:06:08 +0400

    Add tarot cross-link to main menu

--- max_keyboards/main_menu.py
+++ max_keyboards/main_menu.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from maxapi.types import CallbackButton
+from maxapi.types import CallbackButton, LinkButton
 from maxapi.types.attachments.attachment import Attachment
 from maxapi.utils.inline_keyboard import InlineKeyboardBuilder
 
@@ -8,6 +8,7 @@ from maxapi.utils.inline_keyboard import InlineKeyboardBuilder
 def main_menu_attachments() -> list[Attachment]:
     kb = InlineKeyboardBuilder()
     kb.row(CallbackButton(text="🌙 Разобрать сон", payload="menu:ask"))
+    kb.row(LinkButton(text="🔮 Сделать расклад", url="https://max.ru/id644009650098_bot?start=sonik"))
     kb.row(CallbackButton(text="💳 Баланс / Подписка", payload="menu:balance"))
     kb.row(CallbackButton(text="❓ Помощь", payload="menu:help"))
     kb.row(CallbackButton(text="🤝 Пригласить друга", payload="menu:invite"))
